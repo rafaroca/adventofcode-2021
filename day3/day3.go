d6d6d1f6a2f6541637c3a97b30c46b7855a7fe20
Raffael Stein <raffael.stein@codecentric.de>
2021-12-03 17:42:37 +0100

    day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -1,6 +1,9 @@
 package day3
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func Part1(input []string) int {
 	bitCount := len(input[0])
@@ -32,5 +35,68 @@ func Part1(input []string) int {
 }
 
 func Part2(input []string) int {
-	return 0
+	bitCount := len(input[0])
+	fmt.Printf("Running on %d bits.\n", bitCount)
+
+	remainingCandidates := input
+
+	for i := 0; i < bitCount; i++ {
+		bitSum := sumOfBitsAtPosition(remainingCandidates, i)
+		var bit string
+		if bitSum >= 0 {
+			bit = "1"
+		} else {
+			bit = "0"
+		}
+		remainingCandidates = FilterCandidates(remainingCandidates, i, bit)
+		if len(remainingCandidates) == 1 {
+			fmt.Println("Went through", i, "bits to filter until", remainingCandidates)
+			break
+		}
+	}
+
+	oxygen, _ := strconv.ParseInt(remainingCandidates[0], 2, 32)
+
+	remainingCandidates = input
+
+	for i := 0; i < bitCount; i++ {
+		bitSum := sumOfBitsAtPosition(remainingCandidates, i)
+		var bit string
+		if bitSum >= 0 {
+			bit = "0"
+		} else {
+			bit = "1"
+		}
+		remainingCandidates = FilterCandidates(remainingCandidates, i, bit)
+		if len(remainingCandidates) == 1 {
+			fmt.Println("Went through", i, "bits to filter until", remainingCandidates)
+			break
+		}
+	}
+
+	co2, _ := strconv.ParseInt(remainingCandidates[0], 2, 32)
+
+	return int(oxygen * co2)
+}
+
+func FilterCandidates(candidates []string, index int, bit string) []string {
+	var result []string
+	for _, v := range candidates {
+		if string(v[index]) == bit {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func sumOfBitsAtPosition(candidates []string, i int) int {
+	var result int
+	for _, v := range candidates {
+		if v[i] == '1' {
+			result++
+		} else {
+			result--
+		}
+	}
+	return result
 }
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -4,8 +4,21 @@ import "testing"
 
 var test_input = []string{"00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"}
 
-func TestDay3(t *testing.T) {
+func TestDay3Part1(t *testing.T) {
 	if result := Part1(test_input); result != 198 {
 		t.Errorf("The result should be 198 but was '%d'", result)
+	}
+}
+
+func TestFilterCandidates(t *testing.T) {
+	result := FilterCandidates([]string{"001", "101", "111"}, 0, "1")
+	if len(result) != 2 || result[0] != "101" || result[1] != "111" {
+		t.Error("Should only consist of 101 and 111, but is ", result)
+	}
+}
+
+func TestDay3Part2(t *testing.T) {
+	if result := Part2(test_input); result != 230 {
+		t.Errorf("The result should be 230 but was '%d'", result)
 	}
 }
