9261e637bc667c93f1318610a207b60ac476eb95
Raffael Stein <raffael.stein@codecentric.de>
2021-12-04 12:26:10 +0100

    Add day4 input

---
+++ day4/day4.go
@@ -0,0 +1,20 @@
+package day4
+
+type Day4 struct{}
+
+func (t Day4) InputFilename() string {
+	return "day4/input"
+}
+
+func (t Day4) Part1(input []string) int {
+	// read input into stream of numbers and fields
+	// play a game
+	// mark played fields
+	// check for winning field
+	// calculate score of winning field
+	return 0
+}
+
+func (t Day4) Part2(input []string) int {
+	return 0
+}
