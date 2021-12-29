132d1d4c6bac7cdf906de5eaba7196970f49eab7
Raffael Stein <raffael.stein@codecentric.de>
2021-12-29 22:51:10 +0100

    Prepare day 8

---
+++ day8/day8.go
@@ -0,0 +1,15 @@
+package day8
+
+type Day struct{}
+
+func (t Day) Part1(input []string) int {
+	return 0
+}
+
+func (t Day) Part2(input []string) int {
+	return 0
+}
+
+func (t Day) InputFilename() string {
+	return "day8/input"
+}
---
+++ day8/day8_test.go
@@ -0,0 +1,45 @@
+package day8
+
+import (
+	"strings"
+	"testing"
+)
+
+var day = Day{}
+
+func TestPart1(t *testing.T) {
+	result := day.Part1(strings.Split(testInput, "\n"))
+	if result != 26 {
+		t.Error("digits in the output values should be 26 but were", result)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := day.Part2(strings.Split(testInput, "\n"))
+	if result != 0 {
+		t.Error("digits in the output values should be ? but were", result)
+	}
+}
+
+const testInput = `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb |
+fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |
+fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef |
+cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega |
+efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga |
+gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf |
+gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf |
+cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd |
+ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |
+gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |
+fgae cfgab fg bagce
+
+`
